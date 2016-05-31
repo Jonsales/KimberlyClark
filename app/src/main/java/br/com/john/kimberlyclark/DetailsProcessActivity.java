cb145fa75909bc84a2149a2cd38160f9ab58c3b9
Jonsales <jonathan@gtacsolutions.com>
2016-05-31 11:58:52 -0300

    update_layout_final

--- app/src/main/java/br/com/john/kimberlyclark/DetailsProcessActivity.java
+++ app/src/main/java/br/com/john/kimberlyclark/DetailsProcessActivity.java
@@ -1,5 +1,6 @@
 package br.com.john.kimberlyclark;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -7,20 +8,29 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.eazegraph.lib.charts.ValueLineChart;
 import org.eazegraph.lib.models.ValueLinePoint;
 import org.eazegraph.lib.models.ValueLineSeries;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import br.com.john.kimberlyclark.AdpterLists.AdapterListDetails;
 import br.com.john.kimberlyclark.AdpterLists.AdapterListHistoric;
@@ -37,6 +47,7 @@ public class DetailsProcessActivity extends AppCompatActivity {
     public static EditText editStatus, editLimits, editComments;
     public static ImageView imgButtonPrevious, imgButtonNext;
     Button buttonHistoric, buttonHistoricInfo;
+    Spinner spinnerType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +81,8 @@ public class DetailsProcessActivity extends AppCompatActivity {
 
         linearInfoActivity.setOnClickListener(hideInfo);
         textCancel.setOnClickListener(hideInfo);
+
+        initCustomSpinner();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -105,7 +118,6 @@ public class DetailsProcessActivity extends AppCompatActivity {
         AdapterListDetails adapter = new AdapterListDetails(DetailsProcessActivity.this, values, listDetails);
 
         listView.setAdapter(adapter);
-
     }
 
 
@@ -132,5 +144,95 @@ public class DetailsProcessActivity extends AppCompatActivity {
     @Override
     public void onBackPressed(){
         DetailsProcessActivity.this.finish();
+    }
+
+
+
+
+    //**CUSTOM SPINNER
+
+
+    private void initCustomSpinner() {
+
+        Spinner spinnerCustom= (Spinner) findViewById(R.id.spinner_types);
+        // Spinner Drop down elements
+        ArrayList<String> languages = new ArrayList<String>();
+        languages.add("Janeiro");
+        languages.add("Fevereiro");
+        languages.add("Março");
+        languages.add("Abril");
+        languages.add("Maio");
+        CustomSpinnerAdapter customSpinnerAdapter=new CustomSpinnerAdapter(DetailsProcessActivity.this,languages);
+        spinnerCustom.setAdapter(customSpinnerAdapter);
+        spinnerCustom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+
+    public class CustomSpinnerAdapter extends BaseAdapter implements SpinnerAdapter {
+
+        private final Context activity;
+        private ArrayList<String> asr;
+
+        public CustomSpinnerAdapter(Context context,ArrayList<String> asr) {
+            this.asr=asr;
+            activity = context;
+        }
+
+
+
+        public int getCount()
+        {
+            return asr.size();
+        }
+
+        public Object getItem(int i)
+        {
+            return asr.get(i);
+        }
+
+        public long getItemId(int i)
+        {
+            return (long)i;
+        }
+
+
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            TextView txt = new TextView(DetailsProcessActivity.this);
+            final float scale = getResources().getDisplayMetrics().density;
+            txt.setPadding((int)(20 * scale + 0.5f), (int)(10 * scale + 0.5f),
+                    (int)(20 * scale + 0.5f), (int)(10 * scale + 0.5f));
+            txt.setTextSize(16);
+            txt.setGravity(Gravity.LEFT);
+            txt.setText(asr.get(position));
+            txt.setTextColor(getResources().getColor(R.color.color_style_default));
+            return  txt;
+        }
+
+        public View getView(int i, View view, ViewGroup viewgroup) {
+            TextView txt = new TextView(DetailsProcessActivity.this);
+            txt.setGravity(Gravity.CENTER);
+            final float scale = getResources().getDisplayMetrics().scaledDensity;
+            txt.setPadding(0, 0,
+                    (int)(5 * scale + 0.5f), 0);
+            txt.setTextSize(16);
+            txt.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.icon_dropdown_spinner, 0);
+            txt.setText(asr.get(i));
+            txt.setSingleLine(true);
+            txt.setTextColor(getResources().getColor(R.color.color_style_default));
+            return  txt;
+        }
+
     }
 }
